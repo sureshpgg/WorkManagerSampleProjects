cd702aea9e11036d47c13b0d9bb58b80adee61af
sureshpgg <sureshpgg1@gmail.com>
2019-12-19 17:16:12 +0530

    Change one timework request queue to Periodic TimeWork request

--- app/src/main/java/com/example/workmanager_basic_function/MainActivity.java
+++ app/src/main/java/com/example/workmanager_basic_function/MainActivity.java
@@ -4,10 +4,12 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.work.OneTimeWorkRequest;
+import androidx.work.PeriodicWorkRequest;
 import androidx.work.WorkInfo;
 import androidx.work.WorkManager;
 
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,33 +17,46 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.util.concurrent.TimeUnit;
+
 public class MainActivity extends AppCompatActivity {
     public static final String MESSAGE_STATUS = "message_status";
-    TextView tvStatus;
-    Button btnSend;
-
+    Button btnEnqueueWork;
+    Button btnCancelWork;
+    TextView tvWorkStatus;
+    PeriodicWorkRequest workRequest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tvStatus = findViewById(R.id.textView);
-        btnSend = findViewById(R.id.button);
+        btnEnqueueWork = findViewById(R.id.buttonEnqueueWork);
+        btnCancelWork = findViewById(R.id.buttonCancelWork);
+        tvWorkStatus = findViewById(R.id.textViewWorkStatus);
+
 
         final WorkManager mWorkManager = WorkManager.getInstance();
-        final OneTimeWorkRequest mRequest = new OneTimeWorkRequest.Builder(NotificationWorker.class).build();
-        btnSend.setOnClickListener(new View.OnClickListener() {
+       // if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            workRequest = new PeriodicWorkRequest.Builder(NotificationWorker.class, 15, TimeUnit.MINUTES).build();
+     //   }
+        btnEnqueueWork.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mWorkManager.enqueue(mRequest);
+                mWorkManager.enqueue(workRequest);
+            }
+        });
+        btnCancelWork.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                WorkManager.getInstance().cancelWorkById(workRequest.getId());
             }
         });
 
-        mWorkManager.getWorkInfoByIdLiveData(mRequest.getId()).observe(this, new Observer<WorkInfo>() {
+        mWorkManager.getWorkInfoByIdLiveData(workRequest.getId()).observe(this, new Observer<WorkInfo>() {
             @Override
             public void onChanged(@Nullable WorkInfo workInfo) {
                 if (workInfo != null) {
                     WorkInfo.State state = workInfo.getState();
-                    tvStatus.append(state.toString() + "\n");
+                    tvWorkStatus.append(state.toString() + "\n");
 
                 }
             }
